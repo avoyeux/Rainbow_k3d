718f66a6f614068e3c8fa33afcf34fa87f4202dc
Voyeux Alfred <alfredvoyeux@hotmail.com>
2024-01-11 18:36:54 +0100

    added some python codes to do some statistics on the voxels counts for each cube to look for possible periodicities

---
+++ k3d_voxel_stats.py
@@ -0,0 +1,112 @@
+"""
+To do some stats on the voxels from Animation_3D_main.py.
+It is general stats to try and find a periodicity in the total volume of the pixels, on plasma "rain"
+velocity and so on.
+"""
+
+# Imports 
+import os
+import re
+import numpy as np
+import pandas as pd
+
+from pathlib import Path
+from sparse import COO, concatenate, stack
+from typeguard import typechecked
+from Animation_3D_main import Data, CustomDate
+
+
+class Stats(Data):
+    """
+    To do some initial stats.
+    """
+
+    @typechecked
+    def __init__(self, kwargs: dict = {}, everything: bool = True):
+
+        super().__init__(everything, both_cubes=True, **kwargs)
+        self.Structure()
+
+    def Pre_processing(self, cubes):
+        """
+        To process the data so that even the None values become COO matrices
+        """
+
+        if isinstance(cubes, list):
+            shape = (self.cubes_shape[1], self.cubes_shape[2], self.cubes_shape[3]) 
+            print(f'the initial cube shape is {shape}')
+            data = np.array([])
+            coords = np.empty((0, shape[0]))
+            empty_coo = COO(data=data, coords=coords, shape=shape)
+            # empty_coo = COO(data=[], coords=[], shape=shape)
+            print(f' empty coo has shape {empty_coo.shape} and is {empty_coo}')
+            print(f'it has coords {empty_coo.coords} and data {empty_coo.data}')
+            for loop in range(len(cubes)):
+                if cubes[loop] is None:
+                    cubes[loop] = empty_coo
+            result = stack(cubes, axis=0)
+            print(f'the shpae of the concatenate is {result.shape}')
+            return result
+        else:
+            print(f"the initial cube ain't a list. it's shape is {cubes.shape}")
+            return cubes
+
+    def Calculations(self, cubes):
+        """
+        Has the different calculations done on a given cube set
+        """
+        
+        cubes = self.Pre_processing(cubes)
+        summing = COO.sum(cubes, axis=(1, 2, 3))
+        return summing.todense()
+    
+    def Structure(self):
+        """
+        Creation of the data dictionary and hence the corresponding .csv file.
+        """
+
+        data_dict = {'dates (seconds)': self.Dates_sub(),
+                     'all_data_alf': self.Calculations(self.cubes_all_data_1),
+                     'all_data_kar': self.Calculations(self.cubes_all_data_2),
+                     'no_duplicates_stereo_alf': self.Calculations(self.cubes_no_duplicates_STEREO_1),
+                     'no_duplicates_stereo_kar': self.Calculations(self.cubes_no_duplicates_STEREO_2),
+                     'no_duplicates_sdo_alf': self.Calculations(self.cubes_no_duplicates_SDO_1),
+                     'no_duplicates_sdo_kar': self.Calculations(self.cubes_no_duplicates_SDO_2),
+                     'no_duplicates_alf': self.Calculations(self.cubes_no_duplicate_1),
+                     'no_duplicates_kar': self.Calculations(self.cubes_no_duplicate_2)}
+        
+        df = pd.DataFrame(data_dict)
+        df.to_csv('../k3d_stats.csv', index=False)
+
+
+    def Dates_sub(self):
+        """
+        To get the dates of every cube in seconds starting from the first instance, i.e 00:00:00 on the 23-07-2012.
+        """
+
+        path = '../STEREO/avg/'
+        avg_paths = [filepath for filepath in sorted(Path(path).glob('*.png'))]
+        avg_pattern = re.compile(r'''(?P<number>\d{4})_
+                                 (?P<date>\d{4}-\d{2}-\d{2}T\d{2}-\d{2}-\d{2})''', re.VERBOSE)
+        
+        dates = []
+        for path in avg_paths:
+            pattern = avg_pattern.match(os.path.basename(path))
+
+            if pattern:
+                date = CustomDate.parse_date(pattern.group('date'))
+                dates.append(self.Date_to_seconds(date))
+            else:
+                raise ValueError(f"avg filename {path} didn't match")
+        return np.array(dates)
+
+    def Date_to_seconds(self, date):
+        """
+        To change a date in day, hour, minute, seconds format to a seconds format.
+        """
+
+        return (((date.day - 23) * 24 + date.hour) * 60 + date.minute) * 60 + date.second
+    
+
+if __name__=='__main__':
+    Stats()
---
+++ stats_to_plot.py
@@ -0,0 +1,91 @@
+"""
+Code to use the .csv stats file to create corresponding plots 
+"""
+
+# Imports
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class Plotting:
+
+    def __init__(self):
+
+        self.Paths()
+        self.Main()
+    
+    def Paths(self):
+        """
+        To create the saving path
+        """
+
+        main_path = '../'
+        self.paths = {'Main': main_path,
+                      'Stats': main_path,
+                      'Saving': os.path.join(main_path, 'Stats_plots')}
+        os.makedirs(self.paths['Saving'], exist_ok=True)
+        
+    def Main(self):
+        """
+        Main structure of the class.
+        """
+
+        # Getting the data
+        df = pd.read_csv(os.path.join(self.paths['Stats'], 'k3d_stats.csv'))
+        self.dates = df['dates (seconds)']
+
+        all_data_alf = df['all_data_alf']
+        all_data_kar = df['all_data_kar']
+        no_duplicates_stereo_alf = df['no_duplicates_stereo_alf']
+        no_duplicates_stereo_kar = df['no_duplicates_stereo_kar']
+        no_duplicates_sdo_alf = df['no_duplicates_sdo_alf']
+        no_duplicates_sdo_kar = df['no_duplicates_sdo_kar']
+        no_duplicates_alf = df['no_duplicates_alf']
+        no_duplicates_kar = df['no_duplicates_kar']
+
+        # Plotting
+        self.Plots(all_data_alf, all_data_kar, ('all_data_alf', 'all_data_kar'))
+        self.Plots(no_duplicates_alf, no_duplicates_kar, ('no_duplicates_alf', 'no_duplicates_kar'))
+        self.Plots(no_duplicates_stereo_alf, no_duplicates_stereo_kar, 
+                   ('no_duplicates_stereo_alf', 'no_duplicates_stereo_kar'))
+        self.Plots(no_duplicates_sdo_alf, no_duplicates_sdo_kar, 
+                   ('no_duplicates_sdo_alf', 'no_duplicates_sdo_kar'))
+        
+    def Plots(self, cubes_1, cubes_2, name):
+        """
+        To plot the values.
+        """
+
+        # Time evolution
+        plt.figure(figsize=(16, 8))
+        plt.scatter(self.dates, cubes_1, label=name[0], marker='x', color='blue')
+        plt.scatter(self.dates, cubes_2, label=name[1], marker='x', color='orange')
+
+        plt.title(f'Time evolution for {name[0]} and {name[1]}')
+        plt.xlabel(f'Time in seconds')
+        plt.ylabel(f'Voxel count')
+        plt.legend()
+        plt.savefig(os.path.join(self.paths['Saving'], f'Time_evo_{name[0][:-3]}.png'), dpi=100)
+
+        # Normalised time evolution
+        norm_1 = cubes_1 / cubes_1.max()
+        norm_2 = cubes_2 / cubes_2.max()
+        plt.figure(figsize=(16, 8))
+        plt.scatter(self.dates, norm_1, label=name[0], marker='x', color='blue')
+        plt.scatter(self.dates, norm_2, label=name[1], marker='x', color='orange')
+
+        plt.title(f'Normalised time evolution for {name[0]} and {name[1]}')
+        plt.xlabel(f'Time in seconds')
+        plt.ylabel(f'Voxel count')
+        plt.legend()
+        plt.savefig(os.path.join(self.paths['Saving'], f'Norm_evo_{name[0][:-3]}.png'), dpi=100)
+
+
+if __name__=='__main__':
+    Plotting()
+
+
+
+
+
