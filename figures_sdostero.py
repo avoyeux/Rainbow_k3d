1e8b97f12ea5f0e5c9218cc947f7197a74320192
Voyeux Alfred <alfredvoyeux@hotmail.com>
2024-01-11 11:11:35 +0100

    improvements for the figures_sdostereo.py code. now it can make the images and the gifs for a given interval and stereo image choice.

--- figures_sdostero.py
+++ figures_sdostero.py
@@ -20,14 +20,13 @@ from typeguard import typechecked
 class ImageFinder:
 
     @typechecked
-    def __init__(self, ints: bool = False, avgs: bool = False, interval: str = '1h'):
+    def __init__(self, ints: bool = True, avg: bool = False, interval: str = '1h'):
         
         # Arguments
-        if not avgs:
-            self.ints = True
-        else:
+        self.ints = ints
+        self.avgs = avg
+        if avg:
             self.ints = False
-            self.avgs = avgs
         self.interval = interval
 
         # Functions
@@ -82,12 +81,20 @@ class ImageFinder:
                                          (?P<hour>\d{2})-
                                          (?P<minute>\d{2})-
                                          \d{2}\.000\.png''', re.VERBOSE)
-        self.screenshot_pattern = re.compile(r'''interval(?P<interval>\d+h|\d+min)_
-                                             \d{4}-\d{2}-
-                                             (?P<day>\d{2})_
-                                             (?P<hour>\d{2})h
-                                             (?P<minute>\d{2})min_
-                                             v(?P<version>\d{1})\.png''', re.VERBOSE)
+        if self.interval in 'noduplication':
+            self.screenshot_pattern = re.compile(r'''(?P<interval>nodupli)_
+                                                 \d{3}_\d{4}-\d{2}-
+                                                 (?P<day>\d{2})_
+                                                 (?P<hour>\d{2})h
+                                                 (?P<minute>\d{2})min_
+                                                 v(?P<version>\d{1})\.png''', re.VERBOSE)
+        else:
+            self.screenshot_pattern = re.compile(r'''interval(?P<interval>\d+h|\d+min|\d+days|nodupli)_
+                                                \d{4}-\d{2}-
+                                                (?P<day>\d{2})_
+                                                (?P<hour>\d{2})h
+                                                (?P<minute>\d{2})min_
+                                                v(?P<version>\d{1})\.png''', re.VERBOSE)
 
     def Main_loop(self):
         """
@@ -95,7 +102,7 @@ class ImageFinder:
         """
 
         self.groups = []
-        for path_sdo in self.sdo_image[1:]:
+        for path_sdo in self.sdo_image[2:]:
             sdo_groups = self.sdo_pattern.match(os.path.basename(path_sdo))
 
             if sdo_groups:
@@ -113,24 +120,23 @@ class ImageFinder:
             screenshot_groups = self.screenshot_pattern.match(os.path.basename(path_screenshot))
 
             if screenshot_groups:
-                day = int(screenshot_groups.group('day'))
-                hour = int(screenshot_groups('hour'))
-                minute = round(int(screenshot_groups('minute')) / 10) * 10 # to match the sdo one
-                sdo_day = int(sdo_groups.group('day'))
-                sdo_hour = int(sdo_groups.group('hour'))
-                sdo_minute = int(sdo_groups.group('minute'))
+                if screenshot_groups.group('interval') == self.interval:
+                    day = int(screenshot_groups.group('day'))
+                    hour = int(screenshot_groups.group('hour'))
+                    minute = round(int(screenshot_groups.group('minute')) / 10) * 10 # to match the sdo one
+                    sdo_day = int(sdo_groups.group('day'))
+                    sdo_hour = int(sdo_groups.group('hour'))
+                    sdo_minute = int(sdo_groups.group('minute'))
 
-                if minute==60:
-                    hour += 1
-                    minute = 0
-                    if hour==24:
-                        day += 1
-                        hour = 0
-                if (day==sdo_day) and (hour==sdo_hour) and (minute==sdo_minute):
-                    self.Third_loop(sdo_groups, screenshot_groups)
-                    return
-            else:
-                raise ValueError(f"Screenshot filename {os.path.basename(path_screenshot)} doesn't match")
+                    if minute==60:
+                        hour += 1
+                        minute = 0
+                        if hour==24:
+                            day += 1
+                            hour = 0
+                    if (day==sdo_day) and (hour==sdo_hour) and (minute==sdo_minute):
+                        self.Third_loop(sdo_groups, screenshot_groups)
+                        return
             
     def Third_loop(self, sdo_groups, screenshot_groups):
         """
@@ -154,82 +160,6 @@ class ImageFinder:
             else:
                 raise ValueError(f"Stereo filename {os.path.basename(path_stereo)} doesn't match")
 
-    # def SDO_loop(self):
-    #     """
-    #     Loop on the SDO filenames. Not all will be used in the plots.
-    #     """
-
-    #     self.groups = []
-    #     for path_sdo in self.sdo_image:
-    #         sdo_groups = self.sdo_pattern.match(os.path.basename(path_sdo))
-
-    #         if sdo_groups:
-    #             self.STEREO_loop(sdo_groups)
-
-    #         else:
-    #             raise ValueError(f"The SDO filename {os.path.basename(path_sdo)} doesn't match the usual pattern.")
-            
-    #     self.groups = np.array(self.groups)
-
-    # def STEREO_loop(self, sdo_groups):
-    #     """
-    #     Nested loop for the STEREO filenames matching the SDO filenames.
-    #     """
-
-    #     for path_stereo in self.stereo_image:
-    #         stereo_groups = self.stereo_pattern.match(os.path.basename(path_stereo))
-            
-    #         if stereo_groups:
-    #             found = self.STEREO_loop2(sdo_groups, stereo_groups)
-    #             if found:
-    #                 return
-    #         else:
-    #             raise ValueError(f"The STEREO filename {os.path.basename(path_stereo)} doesn't match the usual pattern.")
-            
-    # def STEREO_loop2(self, sdo_groups, stereo_groups):
-    #     """
-    #     Further filtering the STEREO images.
-    #     """
-
-    #     minutes = round(int(stereo_groups.group('minute')) / 10) * 10  # to match the sdo one
-    #     hours = int(stereo_groups.group('hour'))
-    #     day = int(stereo_groups.group('day'))
-
-    #     if minutes==60:
-    #         hours += 1
-    #         minutes = 0
-    #         if hours==24:
-    #             day += 1
-    #             hours = 0
-
-    #     if (day==int(sdo_groups.group('day'))) and (hours==int(sdo_groups.group('hour'))) and (minutes==int(sdo_groups.group('minute'))):
-    #         self.Screenshot_loop(sdo_groups, stereo_groups)
-    #         return True
-    #     else: 
-    #         return False
-
-
-    # def Screenshot_loop(self, sdo_groups, stereo_groups):
-    #     """
-    #     Nested loop on the Screenshots corresponding to the needed image.
-    #     """
-
-    #     for path_screenshot in self.screenshot:
-    #         screenshot_groups = self.screenshot_pattern.match(os.path.basename(path_screenshot))
-
-    #         if screenshot_groups:
-    #             case1 = (int(screenshot_groups.group('day'))==int(stereo_groups.group('day')))
-    #             case2 = (int(screenshot_groups.group('hour'))==int(stereo_groups.group('hour')))
-    #             case3 = (int(screenshot_groups.group('minute'))==int(stereo_groups.group('minute')))
-
-    #             if case1 and case2 and case3:
-    #                 self.last_screenshot = screenshot_groups
-    #                 self.groups.append([sdo_groups.group(), stereo_groups.group(), screenshot_groups.group()])
-    #                 return 
-    #         else:
-    #             raise ValueError(f"The screenshot filename {os.path.basename(path_screenshot)} doesn't match the usual pattern.")
-    #     self.groups.append([sdo_groups.group(), stereo_groups.group(), self.last_screenshot.group()])
-
     def Multiprocessing(self):
 
         # Multiprocesses hates patterns
@@ -239,7 +169,7 @@ class ImageFinder:
         self.screenshot_pattern = None
         self.stereo_ratio_pattern = None
 
-        pool = mp.Pool(processes=16)
+        pool = mp.Pool(processes=14)
         args = [(group_str,) for group_str in self.groups]
         pool.starmap(self.Plotting, args)
         pool.close()
@@ -254,6 +184,7 @@ class ImageFinder:
         self.Patterns()
         stereo_groups = self.stereo_pattern.match(stereo_str)
 
+        print(f'screen str is {screen_str}')
         sdo_screenshot = (mpimg.imread(os.path.join(self.paths['Screenshots'], screen_str)) * 255).astype('uint8')
         stereo_screenshot = (mpimg.imread(os.path.join(self.paths['Screenshots'], screen_str[:-5] + '1.png')) * 255).astype('uint8')
         screenshot = (mpimg.imread(os.path.join(self.paths['Screenshots'], screen_str[:-5] + '2.png')) * 255).astype('uint8')
@@ -289,37 +220,38 @@ class ImageFinder:
         plt.tight_layout(pad=0.1, h_pad=0.1, w_pad=0.1)
 
         figname = f"Fig_{self.interval}_{stereo_groups.group('number')}.png"
-        plt.savefig(os.path.join(self.paths['Save'], figname), dpi=300)
+        plt.savefig(os.path.join(self.paths['Save'], figname), dpi=250)
         plt.close()
-        print(f"Fig nb{stereo_groups.group('number')} done")
 
 class GIF_making:
 
-    def __init__(self, fps=5):
+    def __init__(self, interval='1h', fps=5, stereo='int'):
         self.fps = fps
+        self.interval = interval
+        self.stereo = stereo
         self.Paths()
         self.GIF()
 
     def Paths(self):
-        main_path = '/home/avoyeux/Desktop/avoyeux'
+        main_path = '../'
 
         self.paths = {'Main': main_path,
-                      'Figures': os.path.join(main_path, 'texture_figures12'),
-                      'GIF': os.path.join(main_path, 'texture_GIF')}
+                      'Figures': os.path.join(main_path, f'texture_both_{self.stereo}'),
+                      'GIF': os.path.join(main_path, 'GIF_both')}
         os.makedirs(self.paths['GIF'], exist_ok=True)
 
     def GIF(self):
-
-        images_path = sorted(Path(self.paths['Figures']).glob('*.png'))
+        images_path = sorted(Path(self.paths['Figures']).glob(f'*_{self.interval}_*.png'))
 
         images = [iio3.imread(image_path) for image_path in images_path]
         print(f'the type of an image is {images[0].dtype}')
         print(f'The total size of the images is {round(np.array(images).nbytes / 2**20, 1)}MB')
 
-        iio3.imwrite(os.path.join(self.paths['GIF'], 'GIF_v4.gif'), images, format='GIF', fps=self.fps)
+        iio3.imwrite(os.path.join(self.paths['GIF'], f'GIF_both_{self.stereo}_{self.interval}_fps{self.fps}.gif'),
+                      images, format='GIF', fps=self.fps)
 
 if __name__=='__main__':
-    ImageFinder(interval='1h')
-    # GIF_making()
+    ImageFinder(interval='nodupli', ints=True)
+    GIF_making(interval='nodupli', stereo='int', fps=10)
 
 
