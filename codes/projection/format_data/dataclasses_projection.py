43d4696a8c70a40f9c7fd5949a6847726dde3790
Voyeux Alfred <alfredvoyeux@hotmail.com>
2025-04-28 17:09:49 +0200

    MINOR: Unused dataclasses added.
    I had added two dataclasses to be able to format properly the final data that I will be getting after the warping treatment (with the angles and arc-length). Still not sure if I will actually use those as the relevant code isn't still properly structured.

--- codes/projection/format_data/dataclasses_projection.py
+++ codes/projection/format_data/dataclasses_projection.py
@@ -13,6 +13,9 @@ from .dataclasses_cubes import CubeInformation
 from .dataclasses_sdo_image import PolarImageInfo
 from .dataclasses_fit_n_envelopes import FitWithEnvelopes
 
+# TYPE ANNOTATIONs
+from typing import Literal
+
 
 
 @dataclass(slots=True, repr=False, eq=False)
@@ -76,3 +79,78 @@ class ProjectionData:
     line_of_sight: ProjectedData | None = None
     fake_data: ProjectedData | None = None
     test_cube: ProjectedData | None = None
+
+
+@dataclass(slots=True, repr=False, eq=False)
+class WarpedInformation:
+    """
+    To store all the warped information data for each date.
+    It also stores the corresponding polynomial fit angles.
+    """
+
+    # todo add the contours also in the integration ?
+
+    # DATA
+    warped_values: np.ndarray
+    angles: np.ndarray
+
+    # METADATA
+    name: str
+    date: str
+    max_arc_length: int | float  # ? which type is it ?
+    integration_type: Literal['mean', 'median'] = 'mean'
+
+    def __post_init__(self) -> None:
+
+        # INTEGRATION
+        self.warped_integration()
+
+    def warped_integration(self) -> None:
+        """
+        To compute the integration of the warped values.
+        The values are saved back into the warped_values attribute.
+
+        Raises:
+            ValueError: if the integration type is not 'mean' or 'median'.
+        """
+
+        if self.integration_type == 'mean':
+            self.warped_values = np.mean(self.warped_values.T, axis=0)
+        elif self.integration_type == 'median':
+            self.warped_values = np.median(self.warped_values.T, axis=0)
+        else:
+            raise ValueError(
+                f"\033[1;31mUnknown integration type: {self.integration_type}. "
+                "Choose between 'mean' and 'median'.\033[0m"
+            )
+
+
+@dataclass(slots=True, repr=False, eq=False)
+class AllWarpedInformation:
+    """
+    To store the warped information for all the dates.
+    """
+
+    # ! make sure that the cadence is the same for all the dates or do a time interpolation later
+
+    # METADATA
+    name: str
+    dates: list[str] = field(init=False)
+
+    # DATA
+    warped_informations: list[WarpedInformation]
+
+    def __post_init__(self) -> None:
+
+        # DATEs
+        self.dates = self.get_dates()
+
+    def get_dates(self) -> list[str]:
+        """
+        To get the dates of the warped information.
+
+        Returns:
+            list[str]: the dates of the warped information.
+        """
+
+        return [warped_information.date for warped_information in self.warped_informations]
